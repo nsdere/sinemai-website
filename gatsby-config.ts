fb2ea10cd7e08dfff224aa279f53403029dfa916
ndere <ndere@adobe.com>
2023-07-22 16:42:28 +0200

    template

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,17 +5,14 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // You can also add new values here to query them like usual
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `sinem-ai title`,
+    siteTitleAlt: `sinem-ai title alt`,
+    siteHeadline: `sinem-ai headline`,
+    siteUrl: `sinemaiwebsitemaster.gatsbyjs.io`,
+    siteDescription: `sinem-ai site description.`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `@sinem.ai`,
   },
   trailingSlash: `never`,
   plugins: [
@@ -36,11 +33,11 @@ const config: GatsbyConfig = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/sinem_ai`,
           },
           {
             name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            url: `sinem.ai`,
           },
         ],
       },
@@ -56,7 +53,7 @@ const config: GatsbyConfig = {
       options: {
         name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
         short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        description: `description.`,
         start_url: `/`,
         background_color: `#fff`,
         // This will impact how browsers show your PWA/website
